f5a0712d9726e06ff6dd672771e18d81f35628c9
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-09-20 15:49:31 +0300

    Add Authroize role Middleware to routes

--- sever/middleware/auth.ts
+++ sever/middleware/auth.ts
@@ -7,10 +7,12 @@ import { redis } from "../utils/redis";
 
 export const isAuthenticated = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
-    const access_token = req.cookies.access_token;
+    const access_token = req.cookies.access_token as string;
 
     if (!access_token) {
-      return next(new Errorhandler("Please login to access this service", 400));
+      return next(
+        new Errorhandler("Please login to access this resource", 400)
+      );
     }
     const decoded = Jwt.verify(
       access_token,
@@ -19,11 +21,27 @@ export const isAuthenticated = CatchAsyncError(
     if (!decoded) {
       return next(new Errorhandler("access token not valid", 400));
     }
-    const user = await redis.get(decoded.id as string);
+    const user = await redis.get(decoded.id);
 
     if (!user) {
       return next(new Errorhandler("User not found", 400));
     }
     req.user = JSON.parse(user);
+    next();
   }
 );
+
+//vlaidate user role
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new Errorhandler(
+          `Role: ${req.user?.role} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
