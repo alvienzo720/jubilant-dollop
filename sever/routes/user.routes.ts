63183bc30114fc9242e22ab6db4b86e4ca42d1de
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-09-21 13:05:50 +0300

    Update User Avatar

--- sever/routes/user.routes.ts
+++ sever/routes/user.routes.ts
@@ -8,20 +8,21 @@ import {
   socialAuth,
   updateAccessToken,
   updatePassword,
+  updateProfilePicture,
   updateUserInfo,
 } from "../controllers/user.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { isAbsolute } from "path";
 
 const userRoutes = express.Router();
 userRoutes.post("/registration", registerUser);
 
 userRoutes.post("/activate-user", activateUser);
 userRoutes.post("/login", loginUser);
-userRoutes.get("/logout", isAuthenticated, authorizeRoles("admin"), logoutUser);
+userRoutes.get("/logout", isAuthenticated, authorizeRoles("user"), logoutUser);
 userRoutes.get("/refreshtoken", updateAccessToken);
 userRoutes.get("/me", isAuthenticated, getUserInfo);
 userRoutes.post("/social-auth", socialAuth);
 userRoutes.put("/update-user-info", isAuthenticated, updateUserInfo);
 userRoutes.put("/update-user-password", isAuthenticated, updatePassword);
+userRoutes.put("/update-user-avatar", isAuthenticated, updateProfilePicture); 
 export default userRoutes;
