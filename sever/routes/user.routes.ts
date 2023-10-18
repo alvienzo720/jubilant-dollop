6763efd9f234d12e82db70bbd213efb3096e21e9
Alvin Mutebi <36954843+alvienzo720@users.noreply.github.com>
2023-10-18 15:47:33 +0300

    Users analytics (#30)
    * Add Update User Role Refactor
    
    * Delete user

--- sever/routes/user.routes.ts
+++ sever/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   activateUser,
+  deleteUser,
   getAllUsers,
   getUserInfo,
   loginUser,
@@ -11,6 +12,7 @@ import {
   updatePassword,
   updateProfilePicture,
   updateUserInfo,
+  updateUserRole,
 } from "../controllers/user.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
@@ -27,4 +29,16 @@ userRoutes.put("/update-user-info", isAuthenticated, updateUserInfo);
 userRoutes.put("/update-user-password", isAuthenticated, updatePassword);
 userRoutes.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
 userRoutes.get("/get-all-users", isAuthenticated, getAllUsers);
+userRoutes.put(
+  "/update-user-role",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  updateUserRole
+);
+userRoutes.delete(
+  "/delete-user/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  deleteUser
+);
 export default userRoutes;
--- sever/services/user.service.ts
+++ sever/services/user.service.ts
@@ -15,3 +15,16 @@ export const getAllUsersService = async (res: Response) => {
   const users = await userModel.find().sort({ createdAt: -1 });
   res.status(201).json({ sucess: true, users });
 };
+
+//update user serverive
+export const updateUserRoleService = async (
+  res: Response,
+  id: string,
+  role: string
+) => {
+  const user = await userModel.findByIdAndUpdate(id, { role }, { new: true });
+  res.status(201).json({
+    sucess: true,
+    user,
+  });
+};
