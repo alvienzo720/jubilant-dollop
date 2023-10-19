92d4e0ec5644a720e8546a9e8bd15af14052c972
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-10-19 15:23:45 +0300

    Add Order,Course and User Analytics Routes and Controllers

--- sever/routes/analytics.routes.ts
+++ sever/routes/analytics.routes.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { getUserAnalytics } from "../controllers/analytics.controller";
+import {
+  getCourseAnalytics,
+  getOrderAnalytics,
+  getUserAnalytics,
+} from "../controllers/analytics.controller";
 const analyticsRouter = express.Router();
 
 analyticsRouter.get(
@@ -8,6 +12,20 @@ analyticsRouter.get(
   isAuthenticated,
   authorizeRoles("admin"),
   getUserAnalytics
+);
+
+analyticsRouter.get(
+  "/get-course-analytics",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getCourseAnalytics
+);
+
+analyticsRouter.get(
+  "/get-order-analytics",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  getOrderAnalytics
 );
 
 export default analyticsRouter;
