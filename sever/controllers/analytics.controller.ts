7a601ded81ec600adf0348b63f88d140a0433a06
Alvin Mutebi <36954843+alvienzo720@users.noreply.github.com>
2023-10-19 15:29:38 +0300

    Analytics (#33)
    * Add Update User Role Refactor
    
    * Delete user
    
    * Add Delete Course Route and Controller
    
    * Add Analytics Util and User Analytics
    
    * Add Order,Course and User Analytics Routes and Controllers

--- sever/controllers/analytics.controller.ts
+++ sever/controllers/analytics.controller.ts
@@ -3,6 +3,8 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import Errorhandler from "../utils/ErrorHandling";
 import { generateLast12MonthData } from "../utils/analytics.generator";
 import userModel from "../models/user.model";
+import CourseModel from "../models/course.model";
+import OrderModel from "../models/order.model";
 
 //get user analytics
 export const getUserAnalytics = CatchAsyncError(
@@ -12,6 +14,28 @@ export const getUserAnalytics = CatchAsyncError(
       res.status(200).json({ success: true, users });
     } catch (error: any) {
       return next(new Errorhandler(error.messsage, 500));
+    }
+  }
+);
+
+export const getCourseAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courses = await generateLast12MonthData(CourseModel);
+      res.status(200).json({ success: true, courses });
+    } catch (error: any) {
+      return next(new Errorhandler(error.messsage, 500));
+    }
+  }
+);
+
+export const getOrderAnalytics = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const orders = await generateLast12MonthData(OrderModel);
+      res.status(200).json({ sucess: true, orders });
+    } catch (error: any) {
+      return next(new Errorhandler(error.message, 500));
     }
   }
 );
